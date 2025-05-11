91d5297ee5fc22528965075b9a664acd3c6fd370
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2025-05-11 09:50:44 +0300

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -36,7 +36,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href="https://drive.google.com/file/d/15A1FbVcoKnltN_SQ4zPy_uXRlHO2b8Bz/view?usp=sharings"
+                href="https://drive.google.com/file/d/1-Gw-gK4egJlv_iPu-tRmPV5L2tq7Lezt/view?usp=drive_link"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
                 target="blank"
               >
