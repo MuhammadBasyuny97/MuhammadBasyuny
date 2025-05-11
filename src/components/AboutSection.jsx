2233a429b6aa0a08738301bf9e69bee9f95328b7
Mohamed Basyuny <muhammadbasyunydev@gmail.com>
2025-05-11 08:57:32 +0300

    first commit

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,103 @@
+import { Briefcase, Code, User } from "lucide-react";
+
+export const AboutSection = () => {
+  return (
+    <section id="about" className="py-24 px-4 relative">
+      {" "}
+      <div className="container mx-auto max-w-5xl">
+        <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+          About <span className="text-primary"> Me</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+          <div className="space-y-6">
+            <h3 className="text-2xl font-semibold">
+              Passionate Software Engineer & Problem Solver
+            </h3>
+
+            <p className="text-muted-foreground">
+              With over 3 years of experience in web development, I specialize
+              in creating responsive, accessible, and performant web
+              applications using modern technologies achieving the optimal
+              solutions.
+            </p>
+
+            <p className="text-muted-foreground">
+              I'm passionate about creating elegant solutions to complex
+              problems, and I'm constantly learning new technologies and
+              techniques to stay at the forefront of the ever-evolving web
+              landscape.
+            </p>
+
+            <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
+              <a href="#contact" className="cosmic-button">
+                {" "}
+                Get In Touch
+              </a>
+
+              <a
+                href="https://drive.google.com/file/d/15A1FbVcoKnltN_SQ4zPy_uXRlHO2b8Bz/view?usp=sharings"
+                className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
+                target="blank"
+              >
+                Download CV
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-1 gap-6">
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Code className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">
+                    {" "}
+                    Full-Stack Development
+                  </h4>
+                  <p className="text-muted-foreground">
+                    As a web developer, I specialize in creating dynamic and
+                    responsive websites using a combination of frameworks and
+                    libraries
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <User className="h-6 w-6 text-primary" />
+                </div>
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">Testing</h4>
+                  <p className="text-muted-foreground">
+                    Employing a comprehensive approach to software testing,
+                    encompassing unit, integration, and end-to-end testing.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className="p-3 rounded-full bg-primary/10">
+                  <Briefcase className="h-6 w-6 text-primary" />
+                </div>
+
+                <div className="text-left">
+                  <h4 className="font-semibold text-lg">
+                    Devops & Integration
+                  </h4>
+                  <p className="text-muted-foreground">
+                    Embracing DevOps principles to streamline the development
+                    lifecycle, besides integrating third-party services,
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
